a7d0c550499dd83f43503ca4459e5e67a65ac8a6
Max Burrows <maxajburrows@gmail.com>
2023-07-06 00:33:35 +0200

    Add bootstrap

--- frontend/vite-project/src/App.tsx
+++ frontend/vite-project/src/App.tsx
@@ -2,29 +2,31 @@
 import './App.css'
 import axios from "axios";
 import {useEffect, useState} from "react";
+import {Button} from "react-bootstrap";
 
 function App() {
 
-    const [message, setMessage] = useState<string>('')
-    const getDataFromBackend = async () => {
-        try {
-            setMessage((await axios.get(`http://localhost:8080/api/travelCO2`)).data);
-            console.log(message)
-        } catch (err) {
-            console.error(err)
-        }
-    }
-
-        useEffect(() => {
-            getDataFromBackend()
-        }, []);
+    // const [message, setMessage] = useState<string>('')
+    // const getDataFromBackend = async () => {
+    //     try {
+    //         setMessage((await axios.get(`http://localhost:8080/api/travelCO2`)).data);
+    //         console.log(message)
+    //     } catch (err) {
+    //         console.error(err)
+    //     }
+    // }
+    //
+    //     useEffect(() => {
+    //         getDataFromBackend()
+    //     }, []);
 
         return (
 
 
-            <>
-                <p>{message}</p>
-            </>
+
+
+                <Button>Big text ja<Button/>
+
         )
 
 }
